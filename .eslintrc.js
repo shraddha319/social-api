c55cb0cc479a2849dc5ed02873420570a8f26d6c
shraddha319 <shraddha.jain3219@gmail.com>
2021-09-15 18:37:37 +0530

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-console': ['off'],
+    'no-unused-vars': ['warn'],
+    'no-underscore-dangle': ['off'],
+  },
+};
